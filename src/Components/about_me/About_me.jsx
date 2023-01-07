b98c03bef4b0ee25aa568006e1ce4f62b94c75b7
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-01-07 16:09:49 +0100

    Making website responsive

--- src/Components/about_me/About_me.jsx
+++ src/Components/about_me/About_me.jsx
@@ -1,23 +1,42 @@
-import style from "./style/about_me.css"
-import boilerplate from "../../photos/boilerplate.jpg"
+import style from "./style/about_me.css";
+import boilerplate from "../../photos/boilerplate.jpg";
 
-import githubIcon from "../../icons/github-original.svg"
-import linkedinIcon from "../../icons/linkedin-plain.svg"
+import githubIcon from "../../icons/github-original.svg";
+import linkedinIcon from "../../icons/linkedin-plain.svg";
 
 const AboutMe = () => {
-    return (
-        <section className="about-me--container">
-            <div className="about-me--wrapper">
-                <img className="about-me--image" src={boilerplate} alt="boilerplate"></img>
-                <h3 className="header-font">About me</h3>
-                <p className="about-me--paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!</p>
-                <div className="about-me--links">
-                    <a href="https://github.com/LeskiewiczMichal"><img className="about-me--icon" src={githubIcon} alt="github"/></a>
-                    <a href="https://www.linkedin.com/in/michał-leśkiewicz-0b964425b/"><img className="about-me--icon" src={linkedinIcon} alt="linkedin"/></a>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="about-me--container">
+      <div className="about-me--wrapper">
+        <img
+          className="about-me--image"
+          src={boilerplate}
+          alt="boilerplate"
+        ></img>
+        <div className="about-me--text-wrapper">
+          <h3 className="header-font">About me</h3>
+          <p className="about-me--paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            officia saepe vero ducimus sit, facilis blanditiis distinctio
+            eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi
+            ipsa minus quibusdam corporis? Tempore!
+          </p>
+          <div className="about-me--links">
+            <a href="https://github.com/LeskiewiczMichal">
+              <img className="about-me--icon" src={githubIcon} alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/michał-leśkiewicz-0b964425b/">
+              <img
+                className="about-me--icon"
+                src={linkedinIcon}
+                alt="linkedin"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default AboutMe;
--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -7,49 +7,50 @@ const Projects = () => {
   return (
     <section className="projects--wrapper">
       <h3 className="header-font">My work</h3>
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      <Card
-        img={boilerplate}
-        github="google.com"
-        link="google.com"
-        title="Project template"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
-      />
-      
+      <div className="card-grid">
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+        <Card
+          img={boilerplate}
+          github="google.com"
+          link="google.com"
+          title="Project template"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia saepe vero ducimus sit, facilis blanditiis distinctio eligendi obcaecati ipsum, voluptas, recusandae porro commodi nisi ipsa minus quibusdam corporis? Tempore!"
+        />
+      </div>
     </section>
   );
 };
